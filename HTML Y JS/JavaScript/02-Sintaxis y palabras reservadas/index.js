0195c8e9e2e339c11bafb1670831311e3950d31f
R-Alejandro-C <alecolcas@hotmail.com>
2023-02-06 00:45:57 -0500

    02-sintaxis

---
+++ HTML Y JS/JavaScript/02-Sintaxis y palabras reservadas/index.js
@@ -0,0 +1,16 @@
+
+const libro={
+    titulo:"El nombre de viento",
+    autor:"Patrick Rotfut",
+    fecha: Date(01/01/2003),
+    link: "https://www.casadellibro.com/libro-el-nombre-del-viento-saga-cronica-del-asesino-de-reyes-1/9788401337208/1252241"
+}
+const lista= [
+    "Rodrigo Colcas",
+    19,
+    true,
+    Date(12/03/2003),
+    libro
+];
+
+console.log(lista);
