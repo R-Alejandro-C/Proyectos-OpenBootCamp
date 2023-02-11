9c73b3a8fa7f3a36427b93cf77c3d67659aed487
R-Alejandro-C <alecolcas@hotmail.com>
2023-02-11 00:44:50 -0500

    Numeros

---
+++ HTML Y JS/JavaScript/05-Numeros/index.js
@@ -0,0 +1,7 @@
+let alturaCm = 172;
+let alturaM = 1.72;
+let pesoKg = 75.8;
+let alturaMtrsMayor = Math.round(alturaM) 
+let pesoKgMenor =  Math.floor(pesoKg)
+let maximoValor =Number.MAX_SAFE_INTEGER+1
+console.log(maximoValor);
