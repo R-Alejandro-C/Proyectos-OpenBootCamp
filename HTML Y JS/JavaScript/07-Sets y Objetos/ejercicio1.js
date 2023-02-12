4acff7ccccdbd1c53e64210b1389c96491308b31
R-Alejandro-C <alecolcas@hotmail.com>
2023-02-12 17:11:28 -0500

    ejercicio1

---
+++ HTML Y JS/JavaScript/07-Sets y Objetos/ejercicio1.js
@@ -0,0 +1,9 @@
+const family = ["Norma","Alonso","Osbaldo","Rodrigo"]
+console.log(family);
+const setFamily = new Set(family);
+
+console.log(setFamily); 
+
+console.log(setFamily.add("Rodrigo"));
+
+console.log(setFamily.add("Javascript"));
