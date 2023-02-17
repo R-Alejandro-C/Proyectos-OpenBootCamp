5efd3ca0a2646bc88e9718b8e91d12dc58262130
R-Alejandro-C <alecolcas@hotmail.com>
2023-02-17 02:16:28 -0500

    13-EsLint

---
+++ HTML Y JS/JavaScript/13-ESLint/index.js
@@ -0,0 +1,11 @@
+let nombre = "Rodrigo";
+let apellido="Colcas";
+let estudiante = nombre.concat(" ", apellido);
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let numLetras = estudiante.length;
+let primeraLetra= nombre.charAt();
+let ultimaLetra= apellido.charAt(5);
+let eliminarEspacio = estudiante.trim().length;
+let bool = estudiante.includes("estudiante");
+console.log(bool);
