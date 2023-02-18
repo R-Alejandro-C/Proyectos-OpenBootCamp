d439dcfa35147516b9c5568998badb97737c6b6d
R-Alejandro-C <alecolcas@hotmail.com>
2023-02-18 00:41:39 -0500

    14-eventos

---
+++ HTML Y JS/JavaScript/14-Eventos/index.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector(".btn")
+
+btn.addEventListener("click", () => alert("click en el botón"))
+
+
+$( () => {
+
+    $(".btn").click(() => {
+        console.log("Hola, estoy utilizando jQuery");
+    })
+
+})
+
