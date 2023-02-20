b79bcd137cd5a0d3bc1f70720acce489069ca5bb
R-Alejandro-C <alecolcas@hotmail.com>
2023-02-20 00:22:24 -0500

    17-Mapa

---
+++ HTML Y JS/JavaScript/17-Mapa/index.js
@@ -0,0 +1,54 @@
+let map, markers, position;
+
+function initMap(){
+
+     position = {
+        lat: -25.363,
+        lng: 131.044
+    };
+    map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 8,
+        center: position
+    })
+
+    
+markers = [
+    new google.maps.Marker({
+    position: position,
+    map
+}),
+new google.maps.Marker({
+    position:{
+        lat: -25.276947, 
+
+
+        lng: 131.848617
+    },
+    map
+}),
+new google.maps.Marker({
+    position:{
+        lat: -25.276947, 
+
+
+        lng: 132.848617
+    },
+    map
+})
+]
+
+
+console.log(position);
+
+
+
+
+
+
+
+
+
+
+
+
+}
