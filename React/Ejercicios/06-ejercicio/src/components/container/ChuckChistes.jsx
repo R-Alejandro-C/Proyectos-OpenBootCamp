7200b5efd3823b15dfb62801887b20190dba2864
R-Alejandro-C <alecolcas@hotmail.com>
2023-03-22 01:12:32 -0500

    06-ejercicio part2

--- React/Ejercicios/06-ejercicio/src/components/container/ChuckChistes.jsx
+++ React/Ejercicios/06-ejercicio/src/components/container/ChuckChistes.jsx
@@ -5,7 +5,17 @@ import Button from "@mui/material/Button"
 const ChuckChistes = () => {
 
     const [joke, setjoke] = useState(null);
-
+    const [like, setlike] = useState(0);
+    const [dislike, setdislike] = useState(0);
+    
+    const aumentarLikes = ()=>{
+        setlike(like+1)
+        obtainJoke()
+    }
+    const aumentarDisLikes = ()=>{
+        setdislike(dislike+1)
+        obtainJoke()
+    }
     useEffect(()=>{
         obtainJoke()
     },[])
@@ -24,8 +34,15 @@ const ChuckChistes = () => {
         <div>
             <h1>ChuckChistes</h1>
             {joke != null ? (<div>{joke}</div>):(<div></div>)}
-
+            
             <Button variant='contained' onClick={obtainJoke} style={{margin:"15px"}}>Nuevo ChuckChiste</Button>
+
+            <div>cantidad de LIKES: {like}</div>
+            <div>cantidad de DISLIKES: {dislike}</div>
+            <div>
+            <Button variant='contained' color='success' style={{margin:"10px"}} onClick={aumentarLikes}>LIKE</Button>
+            <Button variant='contained' color='error'style={{margin:"10px"}} onClick={aumentarDisLikes}>DISLIKE</Button>
+            </div>
         </div>
     );
 }
