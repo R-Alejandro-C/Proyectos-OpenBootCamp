66dd89eeca115a27e8ab8461e5cd6e8f83793ac4
RODRIGO COLCAS <106366916+R-Alejandro-C@users.noreply.github.com>
2023-12-02 21:32:05 -0500

    .

---
+++ C#/Introduccion/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Coord cordenadas = new Coord(3,1,4);
+Coord coordenadas2 = cordenadas with { X = 2 };
+
+Console.WriteLine(cordenadas.X);
+Console.WriteLine(cordenadas.Y);
+
+Console.WriteLine(coordenadas2);
+
+public struct Coord
+{
+    public Coord(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public float X{get; set;}
+    public float Z{get; set;}
+    public float Y{get; set;}
+
+public override string ToString() => $"({X},{Y},{Z})";
+
+}
